d87c326140906c409edfcad60b5efc835649b256
JYLinOK <jylin1@163.com>
2024-06-25 09:59:29 +0800

    new code

---
+++ code/J3-int-range/src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        // Byte 类型
+        System.out.println("byte 类型的位数 = " + Byte.SIZE);
+        System.out.println("byte 类型的最大值 = " + Byte.MAX_VALUE);
+        System.out.println("byte 类型的最小值 = " + Byte.MIN_VALUE);
+        System.out.println("byte 类型的 BYTES = " + Byte.BYTES);
+        System.out.println("byte 类型的数据类型 = " + Byte.TYPE);
+        System.out.println();
+
+        // Integer 类型
+        System.out.println("Short 类型的位数 = " + Short.SIZE);
+        System.out.println("Short 类型的最大值 = " + Short.MAX_VALUE);
+        System.out.println("Short 类型的最小值 = " + Short.MIN_VALUE);
+        System.out.println("Short 类型的字节数 = " + Short.BYTES);
+        System.out.println("Short 类型的数据类型 = " + Short.TYPE);
+        System.out.println();
+
+        // Integer  类型
+        System.out.println("Integer 类型的位数 = " + Integer.SIZE);
+        System.out.println("Integer 类型的最大值 = " + Integer.MAX_VALUE);
+        System.out.println("Integer 类型的最小值 = " + Integer.MIN_VALUE);
+        System.out.println("Integer 类型的字节数  = " + Integer.BYTES);
+        System.out.println("Integer 类型的数据类型 = " + Integer.TYPE);
+        System.out.println();
+
+        // Long  类型
+        System.out.println("Long 类型的位数 = " + Long.SIZE);
+        System.out.println("Long 类型的最大值 = " + Long.MAX_VALUE);
+        System.out.println("Long 类型的最小值 = " + Long.MIN_VALUE);
+        System.out.println("Long 类型的字节数 = " + Long.BYTES);
+        System.out.println("Long 类型的数据类型 = " + Long.TYPE);
+        System.out.println();
+
+        // Float 类型
+        System.out.println("Float 类型的位数 = " + Float.SIZE);
+        System.out.println("Float 类型的最大值 = " + Float.MAX_VALUE);
+        System.out.println("Float 类型的最小值 = " + Float.MIN_VALUE);
+        System.out.println("Float 类型的字节数 = " + Float.BYTES);
+        System.out.println("Float 类型的数据类型 = " + Float.TYPE);
+        System.out.println();
+
+        // Double 类型
+        System.out.println("Double 类型的位数 = " + Double.SIZE);
+        System.out.println("Double 类型的最大值 = " + Double.MAX_VALUE);
+        System.out.println("Double 类型的最小值 = " + Double.MIN_VALUE);
+        System.out.println("Double 类型的字节数 = " + Double.BYTES);
+        System.out.println("Double 类型的数据类型 = " + Double.TYPE);
+        System.out.println("Double 类型的有限浮点变量可能具有的最大指数 = " + Double.MAX_EXPONENT);
+        System.out.println("Double 类型的有限浮点变量可能具有的最小指数 = " + Double.MIN_EXPONENT);
+        System.out.println("Double 类型的最小正常值 = " + Double.MIN_NORMAL);
+        System.out.println("Double 类型的负无穷大 = " + Double.NEGATIVE_INFINITY);
+        System.out.println("Double 类型的正无穷大 = " + Double.POSITIVE_INFINITY);
+        System.out.println("Double 类型的非数字（Not a Number）的表示 = " + Double.NaN);
+        System.out.println();
+
+        // Character 类型
+        System.out.println("Character 类型的位数 = " + Character.SIZE);
+        System.out.println("Character 类型的最大值 = " + Character.MAX_VALUE);
+        System.out.println("Character 类型的最小值 = " + Character.MIN_VALUE);
+        System.out.println("Character 类型的字节数 = " + Character.BYTES);
+        System.out.println("Character 类型的数据类型 = " + Character.TYPE);
+        System.out.println();
+    }
+}
